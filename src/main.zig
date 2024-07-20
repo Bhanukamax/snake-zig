8f0da18d3f8e0d43adecd0e0f9e67be4f012ffd1
Bhanuka <bhanukamax@gmail.com>
2024-07-20 16:25:34 +0530

    snake

--- src/main.zig
+++ src/main.zig
@@ -1,64 +1,19 @@
 const std = @import("std");
+const snake_ = @import("snake.zig");
 const ray = @cImport({
     @cInclude("raylib.h");
 });
 
 const width = 500;
 const height = 400;
-const speed = 50;
-const size: f32 = 20.0;
 
-var y: f32 = 10;
-var elapsTime: f32 = 0;
-
-const Direction = enum {
-    Up,
-    Down,
-    Left,
-    Right,
-};
-
-const Snake = struct {
-    pos: ray.Vector2,
-    size: ray.Vector2,
-    direction: Direction,
-    pub fn changeDirection(self: *Snake, direction: Direction) void {
-        self.direction = direction;
-    }
-    pub fn moveSnake(self: *Snake, delta: f32) !void {
-        elapsTime += delta;
-        if (elapsTime > 0.2) {
-            switch (self.direction) {
-                Direction.Up => self.pos.y -= size,
-                Direction.Down => self.pos.y += size,
-                Direction.Left => self.pos.x -= size,
-                Direction.Right => self.pos.x += size,
-            }
-            elapsTime = 0;
-        }
-        var buf: [1000]u8 = undefined;
-
-        const formated = try std.fmt.bufPrint(&buf, "direction: {}", .{self.direction});
-        buf[formated.len] = 0;
-
-        ray.DrawText(&buf, 100, 100, 30, ray.GRAY);
-    }
-    pub fn drawSnake(self: *Snake) !void {
-        ray.DrawRectangleV(self.pos, self.size, ray.BLUE);
-    }
-};
-
-fn newSnake() Snake {
-    const sizeVec = ray.Vector2{ .x = size, .y = size };
-    const pos = ray.Vector2{ .x = 20.0, .y = 20.0 };
-    return Snake{ .pos = pos, .size = sizeVec, .direction = Direction.Down };
-}
+const Direction = snake_.Direction;
 
 pub fn main() !void {
     ray.InitWindow(width, height, "Hello");
     defer ray.CloseWindow();
     ray.SetTargetFPS(120);
-    var snake = newSnake();
+    var snake = snake_.newSnake();
 
     while (!ray.WindowShouldClose()) {
         ray.BeginDrawing();
---
+++ src/snake.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const ray = @cImport({
+    @cInclude("raylib.h");
+});
+
+var elapsTime: f32 = 0;
+const speed = 50;
+const size: f32 = 20.0;
+
+pub const Direction = enum {
+    Up,
+    Down,
+    Left,
+    Right,
+};
+
+pub fn newSnake() Snake {
+    const sizeVec = ray.Vector2{ .x = size, .y = size };
+    const pos = ray.Vector2{ .x = 20.0, .y = 20.0 };
+    return Snake{ .pos = pos, .size = sizeVec, .direction = Direction.Down };
+}
+
+pub const Snake = struct {
+    pos: ray.Vector2,
+    size: ray.Vector2,
+    direction: Direction,
+    pub fn changeDirection(self: *Snake, direction: Direction) void {
+        self.direction = direction;
+    }
+    pub fn moveSnake(self: *Snake, delta: f32) !void {
+        elapsTime += delta;
+        if (elapsTime > 0.2) {
+            switch (self.direction) {
+                Direction.Up => self.pos.y -= size,
+                Direction.Down => self.pos.y += size,
+                Direction.Left => self.pos.x -= size,
+                Direction.Right => self.pos.x += size,
+            }
+            elapsTime = 0;
+        }
+        var buf: [1000]u8 = undefined;
+
+        const formated = try std.fmt.bufPrint(&buf, "direction: {}", .{self.direction});
+        buf[formated.len] = 0;
+
+        ray.DrawText(&buf, 100, 100, 30, ray.GRAY);
+    }
+    pub fn drawSnake(self: *Snake) !void {
+        ray.DrawRectangleV(self.pos, self.size, ray.BLUE);
+    }
+};
